28e165137e27d4d4e87145cc11ce752ff231fa03
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-29 14:59:53 -0700

    created seeds for db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- SEED data --
+INSERT INTO burgers (burger_name, devoured) VALUES ("Deluxe", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Special", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("3x3", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Animal Style", false);
