5fd49de269b87e6350f7ae1993ed837b0567bdbf
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-29 16:17:59 -0700

    created burger model

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// import orm
+var orm = require('../config/orm.js');
+
+// create burger object
+var burger = {
+    // select all burgers table data
+    selectAll: function(cb) {
+        orm.selectAll('burgers', function(res) {
+            cb(res);
+        });
+    },
+
+    // the variables cols and vals are arrays
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    // the objValsCols variable specifies columns with associated values
+    updateOne: function(objValsCols, condition, cb) {
+        orm.updateOne('burgers', objValsCols, condition, function(res) {
+            cb(result);
+        });
+    }
+};
+
+// export functions for the controller
+module.exports = burger;
