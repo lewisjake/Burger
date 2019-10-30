5078ee7bd14ce385d17ddb23d3fca170de3ab546
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-29 19:25:51 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,27 @@
+// dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// serves static content from the public directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// override with POST
+app.use(methodOverride('_method'));
+
+// set handlebars as the view
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlesbars');
+
+// import routes and give server access
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
